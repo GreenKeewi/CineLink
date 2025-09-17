7452fb5b9fd928a7633a47c7025ad1aa4339f08e
Lion Onmars <aliononmars07@gmail.com>
2025-09-17 22:59:56 +0000

    same error

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,6 @@
-
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI({apiVersion: 'v1beta'})],
 });
